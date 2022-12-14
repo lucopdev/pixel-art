10d37b14c919ef4f7472b87a10d264ca8d17daf2
lucopdev <lucopdev@gmail.com>
2022-12-14 17:44:55 -0300

    add botão random colors

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const header = document.getElementsByTagName('header');
 const h1 = document.createElement('h1');
 h1.id = 'title';
 h1.style.fontSize = '50px';
-h1.style.color = 'black';
+h1.style.color = '#dddddd';
 h1.innerHTML = 'Paleta de Cores';
 header[0].appendChild(h1);
 
-const pallete = document.createElement('div');
-pallete.id = 'color-palette';
-document.body.appendChild(pallete);
+const palette = document.createElement('div');
+palette.id = 'color-palette';
+document.body.appendChild(palette);
 
 const colors = ['black', 'red', 'blue', 'yellow'];
 for (let index = 0; index < colors.length; index += 1) {
@@ -18,7 +18,11 @@ for (let index = 0; index < colors.length; index += 1) {
   square.style.height = '50px';
   square.style.border = 'solid 1px black';
   square.style.backgroundColor = colors[index];
-  pallete.appendChild(square);
+  palette.appendChild(square);
 }
 
+const btnRandom = document.createElement('button');
+btnRandom.id = 'button-random-color';
+btnRandom.innerHTML = 'Cores aleatórias';
+palette.appendChild(btnRandom);
 
