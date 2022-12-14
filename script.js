16e3d01007a1b34f795f18209ea3420f3c373359
lucopdev <lucopdev@gmail.com>
2022-12-14 16:41:21 -0300

    add div color-pallete

--- script.js
+++ script.js
@@ -1,19 +1,22 @@
-window.onload = () => {
-  const header = document.getElementsByTagName('header');
-  const h1 = document.createElement('h1');
-  h1.id = 'title';
-  h1.style.fontSize = '50px';
-  h1.style.color = 'black';
-  h1.innerHTML = 'Paleta de Cores';
-  header[0].appendChild(h1);
+const header = document.getElementsByTagName('header');
+const h1 = document.createElement('h1');
+h1.id = 'title';
+h1.style.fontSize = '50px';
+h1.style.color = 'black';
+h1.innerHTML = 'Paleta de Cores';
+header[0].appendChild(h1);
 
-  const colors = ['black', 'red', 'blue', 'yellow'];
-  for (let index = 0; index < colors.length; index += 1) {
-    const square = document.createElement('div');
-    square.className = 'colorPallet';
-    square.style.width = '50px';
-    square.style.height = '50px';
-    square.style.backgroundColor = colors[index];
-    document.body.appendChild(square);
-  }
-};
+const pallete = document.createElement('div');
+pallete.id = 'color-pallete';
+document.body.appendChild(pallete);
+
+const colors = ['black', 'red', 'blue', 'yellow'];
+for (let index = 0; index < colors.length; index += 1) {
+  const square = document.createElement('div');
+  square.className = 'color';
+  square.style.width = '50px';
+  square.style.height = '50px';
+  square.style.border = 'solid 1px black';
+  square.style.backgroundColor = colors[index];
+  pallete.appendChild(square);
+}
