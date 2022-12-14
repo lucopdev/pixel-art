c4ee2ad529a400ae0e34ad019f13a80ffa75885f
lucopdev <lucopdev@gmail.com>
2022-12-14 16:09:03 -0300

    add palleta de cores

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ window.onload = () => {
   h1.style.color = 'black';
   h1.innerHTML = 'Paleta de Cores';
   header[0].appendChild(h1);
+
+  const colors = ['black', 'red', 'blue', 'yellow'];
+  for (let index = 0; index < colors.length; index += 1) {
+    const square = document.createElement('div');
+    square.className = 'colorPallet';
+    square.style.width = '50px';
+    square.style.height = '50px';
+    square.style.backgroundColor = colors[index];
+    document.body.appendChild(square);
+  }
 };
