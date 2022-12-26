8128064ce0c31792c4df8f4b40d2cb72d11ee4ec
lucopdev <lucopdev@gmail.com>
2022-12-26 11:02:10 -0300

    refatorada função de selecionar cor

--- script.js
+++ script.js
@@ -2,23 +2,24 @@ const headerDiv = document.createElement('header');
 document.body.appendChild(headerDiv);
 const header = document.getElementsByTagName('header');
 const h1 = document.createElement('h1');
+const section = document.createElement('section');
 let draw = false;
 
-// cria o titulo
+// ---- TITULO -------
 h1.id = 'title';
 h1.innerHTML = 'Paleta de Cores';
 header[0].appendChild(h1);
 
-// cria a section que segura o menu de configuração
-const section = document.createElement('section');
+// -------- CONTAINER DO MENU DE CONFIGURAÇÕES ----------
 section.id = 'configContainer';
 document.body.appendChild(section);
-// cria a div container para a paleta de cores
+
+// -------- CONTAINER DA PALETA DE CORES
 const palette = document.createElement('div');
 palette.id = 'color-palette';
 section.appendChild(palette);
 
-// cria a paleta de cores
+// ------ PALETA DE CORES -----------
 const createColorPalette = () => {
   const colorsPaletteArray = ['black', 'red', 'blue', 'yellow'];
   for (let index = 0; index < colorsPaletteArray.length; index += 1) {
@@ -36,22 +37,21 @@ const createColorPalette = () => {
 };
 createColorPalette();
 
-// cria div para botões
+// ------- DIV DOS BOTÕES --------
 const buttonsDiv = document.createElement('div');
 buttonsDiv.id = 'buttons-div';
 section.appendChild(buttonsDiv);
 
-// cria botão de cores aleatórias
+// ------- CRIA BOTÃO RANDOMCOLORS ----------
 const btnRandom = document.createElement('button');
 btnRandom.id = 'button-random-color';
 btnRandom.innerHTML = 'Cores aleatórias';
 buttonsDiv.appendChild(btnRandom);
 
-// evento para gerar cores automaticamente ao clicar no botão aleatório
+// -------- GERA CORES ALEATÓRIAMENTE -----------
 const square = palette.childNodes;
 const size = palette.childNodes.length;
 let allColors = '';
-
 btnRandom.addEventListener('click', () => {
   const colorArray = [];
   for (let index = 0; index < size; index += 1) {
@@ -69,13 +69,12 @@ btnRandom.addEventListener('click', () => {
   }
 });
 
-// cria botão reset
+// --------- CRIA BOTÃO RESET ------------
 
 const btnReset = document.createElement('button');
 btnReset.id = 'clear-board';
 btnReset.innerHTML = 'Limpar';
 buttonsDiv.appendChild(btnReset);
-
 btnReset.addEventListener('click', () => {
   const pixel = document.getElementsByClassName('pixel');
   for (let index = 0; index < pixel.length; index += 1) {
@@ -87,18 +86,17 @@ btnReset.addEventListener('click', () => {
   document.location.reload();
 });
 
-// cria input para tamanho do pixelboard
+// -------- CRIA INPUT ---------------
 const input = document.createElement('input');
 input.id = 'board-size';
 input.type = 'number';
 input.setAttribute('min', '1');
 buttonsDiv.appendChild(input);
 
-// cria o pixelboard
+// -------- CRIA O PIXEL BOARD -----------
 const mainSquare = document.createElement('div');
 mainSquare.id = 'pixel-board';
 mainSquare.className = 'pixel-board-class';
-
 const createPixel = (numberOfPixels) => {
   const pixel = document.createElement('div');
   pixel.className = 'pixel';
@@ -109,7 +107,6 @@ const createPixel = (numberOfPixels) => {
   mainSquare.style.height = `${numberOfPixels * 42}px`;
   return document.body.appendChild(mainSquare);
 };
-
 const createPixelBoard = (numberOfPixels) => {
   for (let outerIndex = 0; outerIndex < numberOfPixels; outerIndex += 1) {
     for (let index = 0; index < numberOfPixels; index += 1) {
@@ -119,7 +116,7 @@ const createPixelBoard = (numberOfPixels) => {
 };
 createPixelBoard(5);
 
-// função para remover pixelBoard
+// -------- FUNÇÃO PARA REMOVER PIXELBOARD ----------
 const removePixelBoard = () => {
   const pixel = document.getElementsByClassName('pixel');
   while (mainSquare.children.length > 0) {
@@ -129,7 +126,7 @@ const removePixelBoard = () => {
   }
 };
 
-// função para limitar o pixel board
+// --------- DELIMITAR TAMANHO DO PIXELBOARD ----------
 const pixelBoardLimiter = () => {
   if (input.value < 5) {
     input.value = 5;
@@ -144,12 +141,13 @@ const pixelBoardLimiter = () => {
     mainSquare.style.height = '50px';
   }
 };
-// cria um botão VQV que recria o pixelboard
+// ----------- BOTÃO VQV --------------------
 const buttonVQV = document.createElement('button');
 buttonVQV.id = 'generate-board';
 buttonVQV.innerText = 'VQV';
 buttonsDiv.appendChild(buttonVQV);
 
+// -------- BOTÃO VQV REDIMENSIONA O PIXELBOARD ------------
 buttonVQV.addEventListener('click', () => {
   if (input.value.length <= 0) {
     alert('Board inválido!');
@@ -169,14 +167,13 @@ buttonVQV.addEventListener('click', () => {
 // Lógica para salvar a cor no clique
 const color = palette.getElementsByClassName('color');
 let colorSave = color[0].style.backgroundColor;
-
 palette.addEventListener('click', (event) => {
   if (event.target.className.includes('color')) {
     colorSave = event.target.style.backgroundColor;
   }
 });
 
-// lógica para desenhar enquanto estiver com o mousedown
+// --------- LÓGICA PARA PINTAR COM MOUSEDOWN -----------
 mainSquare.addEventListener('mouseover', (event) => {
   if (!draw) {
     return;
@@ -186,14 +183,12 @@ mainSquare.addEventListener('mouseover', (event) => {
     element.style.backgroundColor = colorSave;
   }
 });
-
 mainSquare.addEventListener('mousedown', (event) => {
   const element = event.target;
   if (element.className.includes('pixel')) {
     element.style.backgroundColor = colorSave;
   }
 });
-
 window.addEventListener('mousedown', () => {
   draw = true;
 });
@@ -201,7 +196,7 @@ window.addEventListener('mouseup', () => {
   draw = false;
 });
 
-// resgata as cores do localStorage
+// ------------ RESGATAR PALETTE COLORS DO LOCALSTORAGE -------------
 const colorArray = JSON.parse(localStorage.getItem('colorPalette'));
 if (colorArray !== null && localStorage.key('colorPalette')) {
   for (let index = 0; index < colorArray.length; index += 1) {
@@ -221,7 +216,7 @@ const pixelBoardGetStorage = () => {
 };
 pixelBoardGetStorage();
 
-// cria a função para salvar e recuperar o deseneho atual no LocalStorage
+// ---------- SALVAR E RESTAURAR PINTURA DO LOCALSTORAGE ----------------
 mainSquare.addEventListener('click', () => {
   const pixelsBackgroundArray = [];
   for (let index = 0; index < mainSquare.children.length; index += 1) {
@@ -241,39 +236,22 @@ for (let index2 = 0; index2 < localStorage.length; index2 += 1) {
 }
 
 
-// definir classe Selected apenas para a cor clicada (essa feature é desnecessária pois resolvi o código sem precisar fiz apenas por ser um item obrigatório)
-color[0].addEventListener('click', () => {
-  if (!color[0].className.includes('selected')) {
-    color[0].className += ' selected';
-    color[1].classList.remove('selected');
-    color[2].classList.remove('selected');
-    color[3].classList.remove('selected');
+const selectOneColor = () => {
+  // const colorPalette = document.getElementById('color-palette');
+  const colors = document.getElementsByClassName('color');
+  for (let index = 0; index < colors.length; index += 1) {
+    colors[index].addEventListener('click', (e) => {
+      for (let index2 = 0; index2 < colors.length; index2 += 1) {
+        if (colors[index2].classList.contains('selected')) {
+          colors[index2].classList.remove('selected');
+          e.target.classList.add('selected');
+        }
+        e.target.classList.add('selected');
+      }
+    });
   }
-});
+};
 
-color[1].addEventListener('click', () => {
-  if (!color[1].className.includes('selected')) {
-    color[1].className += ' selected';
-    color[0].classList.remove('selected');
-    color[2].classList.remove('selected');
-    color[3].classList.remove('selected');
-  }
-});
-
-color[2].addEventListener('click', () => {
-  if (!color[2].className.includes('selected')) {
-    color[2].className += ' selected';
-    color[0].classList.remove('selected');
-    color[1].classList.remove('selected');
-    color[3].classList.remove('selected');
-  }
-});
-
-color[3].addEventListener('click', () => {
-  if (!color[3].className.includes('selected')) {
-    color[3].className += ' selected';
-    color[0].classList.remove('selected');
-    color[1].classList.remove('selected');
-    color[2].classList.remove('selected');
-  }
-});
+window.onload = () => {
+  selectOneColor();
+};
